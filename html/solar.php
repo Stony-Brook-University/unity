981da142159fafe3777aa24b22c0e840230e3cf2
bridgar <bridgerhahn@gmail.com>
2014-06-13 11:41:29 -0400

    Fixed some image references and added php files to wolfienet page and solar page

---
+++ html/solar.php
@@ -0,0 +1,370 @@
+<?php include(header.php) ?>
+	
+<div id="banner"><div id="banner-image"><img src="images/solar-logo.png" /></div><div id="login-button"><a href="http://blackboard.stonybrook.edu">Login to SOLAR <span class="icon-arrow-right icon right"></span></a></div></div>
+			
+						
+					
+			
+	<div id="main-wrap" class="clearfix">
+		
+
+
+		<div id="main-content">
+	 
+		<div id="system-status-message" class="service-disruptions heading-separator">
+		<div class="status-heading"><span class="icon-cancel-circle icon left"></span>Service Disruptions have been reported. </div>
+		<div id="system-status-buttons"><a href="#"><span class="icon-history icon left"></span>View History</a><a href="#"><span class="icon-pushpin icon left"></span>Subscribe</a></div>
+		<div class="status-message">
+		
+		<p>SOLAR is currently experiencing system disruptions. Our IT Staff has been notified and are currently investigating the cause of these outages. <a href="#">View More Details</a></p>
+		
+		<div class="time">
+		<strong class="first">Last Updated:</strong> 2 hours ago<strong>Reported On:</strong> May 27th, 1:18pm<strong>Estimated Update:</strong> About an Hour
+		</div>
+		</div>
+		
+		</div>
+		
+		<div id="system-status-message" class="scheduled-maintenance heading-separator">
+		<div class="status-message"><span class="icon-calendar icon left"></span>This system is scheduled for maintenance on July 28th from 6:00am to 7:00am</div>
+		
+		
+		</div>
+   
+			<div id="overview" class="section">
+		  
+		  <div class="heading-separator"><h2>What Is SOLAR?</h2></div>
+		  
+				<div class="section-contents" id="introduction">
+				
+			
+				<p>SOLAR is Stony Brook University's enterprise-wide, self-service system which provides faculty, staff, and students with online access to manage personal information.</p>
+
+				<p>Students use SOLAR to register for classes, print schedules, view and pay bills, update personal contact information, view transcripts, and submit student employment timesheets.</p>
+				<p>Employees use SOLAR to update personal contact information, view vacation/sick accruals, print class rosters, submit grades, and much more.</p>
+				<p>Anyone can use SOLAR to view our course catalog or search our offered classes.</p>
+				
+				
+				</div>
+
+			</div>
+			
+		
+			
+			<div id="support" class="section gray three-cards">
+			
+				<div class="heading-separator"><h2><span class="icon-support icon left"></span>Getting Support</h2></div>
+				<div class="section-contents">
+				<div id="supported-by" class="text-card card">
+			
+					<h2 class="pane-title">Training</h2>
+					  
+					  <p>
+					  Training is available for faculty and staff SOLAR through the DoIT Training Office.  
+					 </p>
+					 
+					 <p>
+					<strong>Upcoming Trainings</strong>
+					<p>
+					June 6th, 2PM - How to Record Your Timesheet<br />
+					June 12th, 2PM - How to Approve Your Employees Timesheets<br />
+					June 18th, 2PM - How to View Your Courses<br />
+					<a href="#"><strong>View Complete Listing</strong></a>
+					</p>
+					</p>
+					 <strong>Workshops</strong>
+					 <p>In addition to our normally scheduled trainings, you can browse our workshop catalog and request a specific training be offered to you and your staff.</p>
+					
+				
+				</div>
+				
+				<div id="supported-by" class="text-card card">
+			
+					<h2 class="pane-title">Technical Support</h2>
+					  
+					  <p>
+					  Technical Support for SOLAR is handled by Client Support. 
+					 </p>
+					 <span class="icon-phone icon left"></span><span class="visually-hidden">phone</span> 631-632-9800<br /><span class="icon-envelope icon left"></span><span class="visually-hidden">e-mail</span>supportteam@stonybrook.edu<br />
+					 <p>
+					 <strong>Hours:</strong><br />
+					Monday - Friday 9AM-5PM<br />
+					</p>
+					 
+					
+				
+				</div>
+				
+				<div class="text-card card">
+						<h2 class="pane-title">Report a Problem</h2>
+					  
+					  <p>
+					  Difficulty logging in? Not seeing the course content you should? The best way to get help quickly is to submit a ticket and let us know!
+					  </p>
+					  <a href="#" class="report-problem-button"><span class="icon-notification icon left red"></span>Report a Problem</a>
+				
+				</div>
+				
+				
+				
+			</div>
+			</div>
+			
+				<div id="news" class="section three-cards">	 
+				<div class="heading-separator"><h2><span class="icon-newspaper left icon"></span>Latest News & Announcements</h2></div>
+				<div class="section-contents">
+				<div id="cardsWrapper">
+				
+					<div class="cardWrapper image-card card">
+						<div class="cardContentWrapper"><a href="http://www.stonybrook.edu/commcms/global"> <img src="images/solar-password.png" alt="Gloabl Initiatives"></a>
+						<h3>DoIT Calls for Password Changes in Response to Heartbleed</h3><p>CIO Cole Camplese issued a campus announcement on April 15, urging everyone to change their NetID passwords as a result of the Heartbleed Bug.</p>
+						</div>
+					</div>
+
+					<div class="cardWrapper image-card card">
+					<div class="cardContentWrapper"><a href="http://www.stonybrook.edu/commcms/global"> <img src="images/sb-alert.png" alt="Gloabl Initiatives"></a>
+					
+					
+					
+					<h3>Subscribe to SB Alert</h3><p>Get updates on major campus emergencies, immediate threats or impending situations that could pose harm, disrupt classes, impact facilities, activities, or other operations.</p>
+					</div>
+					</div>
+
+					<div class="cardWrapper image-card card">
+					<div class="cardContentWrapper"><a href="http://www.stonybrook.edu/commcms/global"> <img src="images/solar-mobile.png" alt="Gloabl Initiatives"></a>
+					<h3>SOLAR Mobile</h3><p>SOLAR is now available on your mobile devices. Making it easier than ever to stay connected to your coursework.</p>
+					</div>
+					</div>
+
+				</div>
+				</div>
+			</div>
+		
+		
+		
+		
+			<div id="explore" class="section six-cards gray">	 
+				<div class="heading-separator"><h2><span class="icon-map left icon"></span>Explore SOLAR</h2></div>
+				<div class="section-contents">
+				<div id="cardsWrapper">
+				
+						<div class="cardWrapper text-card card">
+							<h3>Security and Personal Data</h3><p>Update your preferred contact information and manage your passwords.</p>
+						
+						</div>
+
+						<div class="cardWrapper text-card card">
+						<h3>Student Records & Registration</h3><p>Lookup courses and build your schedule</p>
+						
+						</div>
+
+						<div class="cardWrapper text-card card">
+							<h3>Employment Services</h3><p>Full-time, part-time and student employees can enter timesheets, view paychecks and monitor their leave.</p>
+						</div>
+						
+						<div class="cardWrapper text-card card">
+							<h3>Financial Services</h3><p>Financial Services are lots of fun. </p>
+						</div>
+						
+						<div class="cardWrapper text-card card">
+							<h3>Housing Services</h3><p>Financial Services are lots of fun. </p>
+						</div>
+						
+						<div class="cardWrapper text-card card">
+							<h3>SB Alert</h3><p>Get updates on major campus emergencies,  immediate threats or impending situations that could pose harm, disrupt classes, impact facilities, activities, or other operations.</p>
+						</div>
+						
+						<div class="cardWrapper text-card card">
+							<h3>Message Center</h3><p>Be sure to check your SOLAR message center often and take action if needed.</p>
+						</div>
+						
+						<div class="cardWrapper text-card card">
+							<h3>Public Course Catalog</h3><p>View course details and descriptions.</p>
+						</div>
+						
+
+						<div class="cardWrapper text-card card">
+							<h3>Class Schedules</h3><p>View what classes are currently being offered.</p>
+						</div>
+						
+					</div>
+
+				</div>
+			</div>
+			
+			
+		
+
+			
+			
+
+			
+
+			
+	
+			<div class="section" id="faq">
+			<div class="heading-separator"><h2><span class="icon-question left icon"></span>Frequently Asked Questions</h2></div>
+			<div class="section-contents">
+			<h2>For Everyone</h2>
+			<div class="accordion">
+  <h3>How Do I Login to SOLAR?</h3>
+  <div>
+    <p>
+		The first time you log into SOLAR, you will need your 9 digit Stony Brook University ID Number and your birthdate. This information......</a>. 
+    </p>
+  </div>
+  <h3>How can I View My Messages?</h3>
+  <div>
+    <p>
+    If the answer can be explained and solved in 2 - 3 paragraphs, without the need of images, the solution should be put here. 
+	</p>
+	<p>
+	If the solution is more complicated, this text should be a 1-3 paragraph introduction about the problem and we'll provide a link to the full solution automatically. 
+	</p>
+	<p>
+	<a href="#">View entire KB Article</a>
+	</p>
+	
+    </p>
+  </div>
+  
+  
+</div>
+<p><a href="#">There are 5 more KB Articles relating to SOLAR for Everyone</a></p>
+			<h2>For Employees</h2>
+			<div class="accordion">
+  <h3>How Do I View My Vacation and Sicktime?</h3>
+  <div>
+  <p>
+    If the answer can be explained and solved in 2 - 3 paragraphs, without the need of images, the solution should be put here. 
+	</p>
+	<p>
+	If the solution is more complicated, this text should be a 1-3 paragraph introduction about the problem and we'll provide a link to the full solution automatically. 
+	</p>
+	<p>
+	<a href="#">View entire KB Article</a>
+	</p>
+  </div>
+  <h3>How Do I Submit My Monthly Timesheet?</h3>
+  <div>
+   <p>
+    If the answer can be explained and solved in 2 - 3 paragraphs, without the need of images, the solution should be put here. 
+	</p>
+	<p>
+	If the solution is more complicated, this text should be a 1-3 paragraph introduction about the problem and we'll provide a link to the full solution automatically. 
+	</p>
+	<p>
+	<a href="#">View entire KB Article</a>
+	</p>
+  </div>
+  <h3>How Can I View My Paycheck Stub?</h3>
+  <div>
+   <p>
+    If the answer can be explained and solved in 2 - 3 paragraphs, without the need of images, the solution should be put here. 
+	</p>
+	<p>
+	If the solution is more complicated, this text should be a 1-3 paragraph introduction about the problem and we'll provide a link to the full solution automatically. 
+	</p>
+	<p>
+	<a href="#">View entire KB Article</a>
+	</p>
+  </div>
+  
+  
+</div>
+
+<p><a href="#">There are 30 more KB Articles relating to SOLAR for Instructors</a></p>
+
+
+			<h2>For Students</h2>
+			<div class="accordion">
+  <h3>How Do I Register for Classes?</h3>
+  <div>
+<p>
+    If the answer can be explained and solved in 2 - 3 paragraphs, without the need of images, the solution should be put here. 
+	</p>
+	<p>
+	If the solution is more complicated, this text should be a 1-3 paragraph introduction about the problem and we'll provide a link to the full solution automatically. 
+	</p>
+	<p>
+	<a href="#">View entire KB Article</a>
+	</p>
+  </div>
+  <h3>How Do I View and Pay My Bills?</h3>
+  <div>
+  <p>
+    If the answer can be explained and solved in 2 - 3 paragraphs, without the need of images, the solution should be put here. 
+	</p>
+	<p>
+	If the solution is more complicated, this text should be a 1-3 paragraph introduction about the problem and we'll provide a link to the full solution automatically. 
+	</p>
+	<p>
+	<a href="#">View entire KB Article</a>
+	</p>
+  </div>
+  <h3>How Do I Register for Housing?</h3>
+  <div>
+   <p>
+    If the answer can be explained and solved in 2 - 3 paragraphs, without the need of images, the solution should be put here. 
+	</p>
+	<p>
+	If the solution is more complicated, this text should be a 1-3 paragraph introduction about the problem and we'll provide a link to the full solution automatically. 
+	</p>
+	<p>
+	<a href="#">View entire KB Article</a>
+	</p>
+  </div>
+  
+  
+</div>
+
+<p><a href="#">There are 12 more KB Articles relating to SOLAR for Students</a></p>
+</div>
+</div>
+
+		  
+		  
+		  
+			
+			
+		  
+				<div id="join-conversation" class="section gray three-cards">
+				<div class="heading-separator"><h2><span class="icon-users left icon"></span>Join The Conversation</h2></div>
+<div class="section-contents">				
+						<div id="twitter" class="text-card card">  
+							<h2 class="pane-title"><span class="icon-twitter icon left"></span>Twitter</h2>
+				  
+							  <p>
+							  Have an opinion about SOLAR? We want to hear about it, tweet us: <a href="https://twitter.com/search?f=realtime&q=%23solar%20to%3Asbudoit&src=typd">@sbudoit #solar</a>
+							  </p>
+						</div>
+		  
+						<div id="supported-by" class="text-card card">
+							<h2 class="pane-title"><span class="icon-bubbles icon left"></span>Yammer</h2>
+					  
+							  <p>
+							Join the Conversation on Yammer, ask questions, get help from DoIT Staff and other members of the community. <a href="some-link">Link to Yammer Group</a>
+							  </p>
+					 
+						</div>
+				
+					<div id="third-box" class="text-card card">
+							<h2 class="pane-title"><span class="icon-bullhorn icon left"></span>Feedback</h2>
+					  
+						  <p>
+						  Have an opinion, but don't want to blast it across our social networks? Drop us a note, <a href="feedback">we love getting feedback</a>.
+						  </p>
+					  
+					</div>
+		  
+			</div>  
+			
+			
+		</div>
+
+	</div>
+
+	
+		 		
+	
+<?php include(footer.php) ?>
