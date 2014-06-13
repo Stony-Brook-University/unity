a2cfb3d7c3ed73231e4fd0f279d899bb7a60bea5
Richard von Rauchhaupt <richard.vonRauchhaupt@stonybrook.edu>
2014-06-13 18:51:37 -0400

    added styleguide and sass updates

---
+++ styleguide/header.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
+  "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" version="XHTML+RDFa 1.0" dir="ltr"
+  xmlns:content="http://purl.org/rss/1.0/modules/content/"
+  xmlns:dc="http://purl.org/dc/terms/"
+  xmlns:foaf="http://xmlns.com/foaf/0.1/"
+  xmlns:og="http://ogp.me/ns#"
+  xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+  xmlns:sioc="http://rdfs.org/sioc/ns#"
+  xmlns:sioct="http://rdfs.org/sioc/types#"
+  xmlns:skos="http://www.w3.org/2004/02/skos/core#"
+  xmlns:xsd="http://www.w3.org/2001/XMLSchema#">
+
+<head profile="http://www.w3.org/1999/xhtml/vocab">
+  
+        <meta http-equiv="pragma" content="no-cache" />
+        <meta http-equiv="expires" content="-1" />  
+		<meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0,
+      width=device-width, user-scalable=no, target-densitydpi=medium-dpi" />
+        
+ <link type="text/css" rel="stylesheet" href="http://fast.fonts.net/cssapi/a8a128e0-bc0f-4153-aa44-ede674f36473.css"/>
+  <title>DoIT Unity Style Guide- Division of Information Technology, Stony Brook University</title>
+<style>
+@import url("http://it.dev.sinc.stonybrook.edu/modules/system/system.base.css?n5ycpv");
+@import url("styles/icomoon-style.css");
+
+@import url("stylesheets/style.css");
+@import url("styles/style.css");
+</style>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+
+</head>
+<body class="html not-front logged-in unity-no-sidebar panel-layout-snape" >
+
+<div id="page" class="clearfix">
+
+ 	<div id="header">
+    			
+		   <a href="http://www.stonybrook.edu" title="Stonybrook University" rel="home" id="university-logo"><img src="http://it.dev.sinc.stonybrook.edu/sites/all/themes/custom/unity/assets/stony-brook-university.png" alt="Stony Brook University Logo" /></a><a id="logo" href="/"><img title="Home" src="http://it.dev.sinc.stonybrook.edu/sites/it.stonybrook.edu/files/doit-logo.png" /></a> 			
+       
+	</div>
+	
+	<div id="main-menu">
+
+	  <div id="main-menu-controller" class="navigation show-phone">
+	  	<ul class="links clearfix">
+	  		<li id="prefix-display-button"><a href="#"><span class="sb-icon-menu"></span> Menu</a></li>
+	  	</ul>
+	  </div>
+	  
+      <div id="main-menu-navigation" class="navigation hide-phone">
+
+			
+    <ul class="menu">
+		<li class="first leaf"><a href="/" title="Home">Styleguide Home</a></li>
+		<li class="leaf"><a href="/blocks.php" title="Home">Blocks</a></li>
+		<li class="leaf"><a href="/text.php" title="Home">Text</a></li>
+		<li class="leaf"><a href="/colors.php" title="Home">Colors</a></li>
+		<li class="leaf"><a href="/layouts.php" title="Home">Layouts</a></li>
+	</ul>
+
+	</div>
+
+
+			<div id="sb-search" class="sb-search">
+				 
+  <div class="content">
+    <form action="/services/wi-fi-wolfienet" method="post" id="search-block-form" accept-charset="UTF-8"><div><div class="container-inline">
+      <h2 class="element-invisible">Search form</h2>
+    <div class="form-item form-type-textfield form-item-search-block-form">
+  <label class="element-invisible" for="edit-search-block-form--2">Search </label>
+ <input title="Enter the terms you wish to search for." class="sb-search-input sb-search-input form-text" placeholder="Search" type="text" id="edit-search-block-form--2" name="search_block_form" value="" size="40" maxlength="128" />
+</div>
+<div class="form-actions form-wrapper" id="edit-actions"><input class="sb-search-submit sb-search-submit form-submit" type="submit" id="edit-submit" name="op" value="" /></div><input type="hidden" name="form_build_id" value="form-9zCmbOkTXu2NDCvb16WTCJ0xUzquLaDIpNrpvkQIl5k" />
+<input type="hidden" name="form_token" value="BySz9EzP8k8rJ_1QEFgClgZb5PYm7sOerrMbCVu--28" />
+<input type="hidden" name="form_id" value="search_block_form" />
+</div>
+</div></form>  </div>
+</div>
+		
+
+</div>
