63496512b1e36e42a1a7f71f23501c8bedade3ff
Richard von Rauchhaupt <rvonrauchhau@doit-comm-01.campus.stonybrook.edu>
2014-12-04 18:56:12 -0500

    Full width reload of unity

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,172 +5,39 @@ module.exports = function (grunt) {
 	//GRUNT TASKS
   grunt.initConfig({
 	
-	//BROWSER SYNC TASK
-	browserSync: {
 	
-		styleguide:
-		{
-				//Files to search for
-			files: {
-				src: 'styleguide/**/*'
-			},
-			//What to do with the files
-			options: {
-				proxy: "styleguide.localhost:8080",
-
-				injectChanges: false,
-				debugInfo: true,
-				logConnections: true,
-				watchTask: true,
-				startPath: '/',
-				ports: {
-					min: 9000,
-					max: 9020
-				},
-
-				host : "styleguide.localhost",
-				ghostMode: {
-					clicks: true,
-					scroll: true,
-					links: true,
-				forms: true
-				}
-			}
-		
-		},
-		
-		classie:
-		{
-				//Files to search for
-			files: {
-				src: 'stylesheets/**'
-			},
-			//What to do with the files
-			options: {
-				proxy: "classie.localhost:8082",
-
-				injectChanges: false,
-				debugInfo: true,
-				logConnections: true,
-				watchTask: true,
-				startPath: '/text.php',
-				ports: {
-					min: 9000,
-					max: 9020
-				},
-
-				host : "classie.localhost",
-				
-				ghostMode: {
-					clicks: true,
-					scroll: true,
-					links: true,
-				forms: true
-				}
-			}
-		
-		},
-
-		it:
-		{
-				//Files to search for
-			files: {
-				src: 'stylesheets/**'
-			},
-			//What to do with the files
-			options: {
-				
-
-				injectChanges: false,
-				debugInfo: true,
-				logConnections: true,
-				watchTask: true,
-				startPath: '/',
-				ports: {
-					min: 9000,
-					max: 9020
-				},
-
-				host : "it.localhost:8080",
-				
-				ghostMode: {
-					clicks: true,
-					scroll: true,
-					links: true,
-				forms: true
-				}
-			}
-		
-		},
-		
-		default: {
-		
-			//Files to search for
-			files: {
-				src: '{,**/}*.css'
-			},
-			//What to do with the files
-			options: {
-				proxy: "styleguide.localhost:8080",
-
-				injectChanges: false,
-				debugInfo: true,
-				logConnections: true,
-				watchTask: true,
-				startPath: '/text.php',
-				ports: {
-					min: 9000,
-					max: 9020
-				},
-
-				host : "styleguide.localhost",
-				ghostMode: {
-					clicks: true,
-					scroll: true,
-					links: true,
-				forms: true
-				}
-			}
-		}
-    },
 	
   
-	//WATCH TASK
+//WATCH TASK
     watch: {
 	
-	  styleguide: {
-		  files: ['sass/{,**/}*.scss', 'images/{,**/}/*', 'local-images/{,**/}*', 'javascripts/{,**/}*.js'],
-		  tasks: ['compass:dev', 'copy:styleguide']
-	 
+	  scss: {
+		  files: ['scss/{,**/}*.scss'],
+		  tasks: ['compass:dev', 'copy:css']
 		},
+  
+	  
+	  js: {
+	     files: ['js/{,**/}/*.js'],
+		 tasks: ['copy:js']
+	  },
 
-		 it: {
-		  files: ['sass/{,**/}*.scss'],
-		  tasks: ['compass:dev', 'copy:stylesheets', 'copy:it']
-	 
-		},
-		
-		sbucommons: {
-		  files: ['sass/{,**/}*.scss'],
-		  tasks: ['compass:dev', 'copy:stylesheets', 'copy:sbucommons']
-	 
-		},
-	
-		sass: {
-			files: ['sass/{,**/}*.scss'],
-			tasks: ['compass:dev', 'copy:stylesheets'], //run compass task for sass files
-		  },
+	  images: {
+	     files: ['images/{,**/}/*'],
+		 tasks: ['copy:images']
+	  },
 
-		  //javascript to watch
-		  js: {
-			files: [
-			  'javascripts/{,**/}*.js',
-			  '!javascripts/{,**/}*.js'
-			],
-			tasks: ['jshint', 'uglify:dev', 'copy:javascripts'] //run jshint and uglify for javascript
-		  }
-	},
+	  fonts: {
+	     files: ['fonts/{,**/}/*'],
+		 tasks: ['copy:fonts']
+	  },
+
 	
+	 },
+
+
+
+
 	//COMPASS TASK
     compass: {
       options: {
@@ -187,211 +54,66 @@ module.exports = function (grunt) {
       dist: {
         options: {
           environment: 'production',
-          imagesDir: 'images-min',
-          force: true
         }
       }
     },
-	
-	//JSHINT TASK
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-	  //Javascript to search for and check
-      all: [
-        'javascripts/{,**/}*.js',
-        '!javascripts/{,**/}*.min.js'
-      ]
-    },
-	
-	//IMAGEMIN TASK
-    imagemin: {
-      dist: {
-        options: {
-          optimizationLevel: 3
-        },
-		//images to search for, compress, and place in the destination
-        files: [{
-          expand: true,
-          cwd: 'images',
-          src: ['**/*.png', '**/*.jpg'],
-          dest: 'images-min/'
-        }]
-      }
-    },
-	
-	//SVGMIN TASK
-    svgmin: {
-	  //DIST SUBTASK
-      dist: {
-        files: [{
-          expand: true,
-          cwd: 'images',
-          src: '**/*.svg',
-          dest: 'images-min'
-        }]
-      }
-    },
-	
-	//UGLIFY TASK
-    uglify: {
-	  //DEV SUBTASK
-      dev: {
-        options: {
-          mangle: false,
-          compress: false,
-          beautify: true
-        },
-		//javascript to look for, minify, and place in the destination
-        files: [{
-          expand: true,
-          cwd: 'javascripts',
-          src: ['**/*.js', '!**/*.min.js'],
-          dest: 'javascripts',
-          ext: '.min.js'
-        }]
-      },
-	  //DIST SUBTASK
-      dist: {
-        options: {
-          mangle: true,
-          compress: true
-        },
-		//javascript to look for, minify, and place in the destination
-        files: [{
-          expand: true,
-          cwd: 'javascripts',
-          src: ['**/*.js', '!**/*.min.js'],
-          dest: 'javascripts',
-          ext: '.min.js'
-        }]
-      }
-    },
+
+ 	
 	
 	//COPY TASK
     copy: {
 	
-	
-	 stylesheets: {
+	  css: {
 	  
 	   //Files to look for and move to the destination
         files: [  
-		   {
-		  expand: true,
-		  cwd: 'stylesheets',
-		  src: ['**'],
-		  dest: 'stylesheets'
-		  }
-        ]
-      },
-	
-	  styleguide: {
-	  
-	   //Files to look for and move to the destination
-        files: [  
-		   {
-		  expand: true,
-		  cwd: 'stylesheets',
-		  src: ['**'],
-		  dest: 'styleguide/stylesheets'
-		  },
-
 		  {
 		  expand: true,
-		  cwd: 'local-images',
+		  cwd: 'css',
 		  src: ['**'],
-		  dest: 'styleguide/local-images'
-		  },
+		  dest: 'release/stylesheets'
+		  } ]
+		},
 
+		images: {
+
+			files: [
 		  {
 		  expand: true,
 		  cwd: 'images',
 		  src: ['**'],
-		  dest: 'styleguide/stylesheets/images'
-		  },
-		  
+		  dest: 'release/stylesheets/images'
+		  }
+		  ]
+		},
+
+			
+		js: {
+			files: [
 		  {
 		  expand: true,
-		  cwd: 'javascripts',
+		  cwd: 'js',
 		  src: ['**'],
-		  dest: 'styleguide/javascripts'
-		  }
+		  dest: 'release/javascripts'
+		  } ]
+		 },
+
+		 fonts: {
+		 	files: [
+		  {
+		  expand: true,
+		  cwd: 'fonts',
+		  src: ['**'],
+		  dest: 'release/stylesheets/fonts'
+		  },
         ]
       },
 
-       it: {
-	  
-	   //Files to look for and move to the destination
-        files: [  
-		   {
-		  expand: true,
-		  cwd: 'stylesheets',
-		  src: ['**'],
-		  dest: 'e:/git/kerrinpernic/docroot/sites/all/themes/custom/unity/stylesheets'
-		  }
-        ]
-      },
-	  
-	  sbucommons: {
-	  
-	   //Files to look for and move to the destination
-        files: [  
-		   {
-		  expand: true,
-		  cwd: 'stylesheets',
-		  src: ['**'],
-		  dest: 'e:/git/sbucommons/docroot/sites/all/themes/custom/unity/stylesheets'
-		  }
-        ]
-      },
-	  
-	  //DIST SUBTASK
-      dist: {
-	    //Files to look for and move to the destination
-        files: [
-          {
-            expand: true,
-            cwd: 'images',
-            src: ['**', '!**/*.svg', '!**/*.png', '!**/*.jpg'],
-            dest: 'images-min'
-          },
-		  
-		  {
-		  expand: true,
-		  cwd: 'stylesheets',
-		  src: ['**'],
-		  dest: 'drupal/stylesheets'
-		  },
-		  
-		   {
-		  expand: true,
-		  cwd: 'stylesheets',
-		  src: ['**'],
-		  dest: 'styleguide/stylesheets'
-		  },
-		  
-		   {
-		  expand: true,
-		  cwd: 'stylesheets',
-		  src: ['**'],
-		  dest: 'html/stylesheets'
-		  }
+       
 
-        ]
-      }
     }
   });
   
-  
-  grunt.event.on('watch:styleguide', function(action, filepath) {
-    grunt.config([
-      'compass:dev',
-      'jshint'
-    ], filepath);
-	
-  });
-
   
   //LOAD PLUGINS
   grunt.loadNpmTasks('grunt-contrib-watch');
@@ -403,15 +125,13 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-bower-concat');
 
-  grunt.registerTask('build', ['parallel', 'compass:dist', 'jshint']);
-  grunt.registerTask('default', ['build', 'browserSync', 'watch']);
-  grunt.registerTask('csswatch', ['browserSync', 'watch']);
+  //grunt.registerTask('build', ['parallel', 'compass:dist', 'jshint']);
+  //grunt.registerTask('default', ['build', 'browserSync', 'watch']);
+
+  grunt.registerTask('default', ['compass:dev', 'copy', 'watch']);
+
+  grunt.registerTask('drupal', ['compass:dev', 'copy:drupal', 'watch']);
     
-  grunt.registerTask('styleguidewatch', ['compass:dev', 'copy:styleguide', 'browserSync:styleguide', 'watch:styleguide']);
-  
-  grunt.registerTask('classiewatch', ['browserSync:classie', 'watch:sbucommons']);
-
-
-  grunt.registerTask('itwatch', ['watch:it']);
 };
--- styleguide/footer.php
+++ styleguide/footer.php
@@ -5,53 +5,40 @@
 		
 	
 			<div id="left-menu-footer" class="sub-footer first odd">
-		    <div id="block-menu-menu-menu-about-us" class="block block-menu contextual-links-region">
-
-    
- <div class="panel-title-div"><h2>Resources</h2></div>
-
-  <div class="content">
+	    
+ <h2>Resources</h2>
+ 
     <ul class="menu"><li class="first leaf"><a href="http://github.com/sbudoit" title="">SBU DoIT Github</a></li>
     	<li><a href="http://slides.com/richardvonrauchhaupt/doit-content-contributors#/">DoIT Content Contributors 2014</a></li>
 
 </ul>  </div>
-</div>
-		</div>
 		
 		
 		<div id="middle-menu-footer" class="sub-footer even">
-			  <div id="block-menu-menu-community" class="block block-menu contextual-links-region">
 
     
- <div class="panel-title-div"><h2>Libraries</h2></div>
+<h2>Libraries</h2>
 	
-  <div class="content">
     <ul class="menu"><li class="first leaf"><a href="#" title="">SASS</a></li>
 <li class="last leaf"><a href="#" title="">Grunt</a></li>
-</ul>  </div>
+</ul>  
+
 </div>
-		</div>
-		
 		
 		<div id="right-menu-footer" class="sub-footer last odd">
-					    <div id="block-menu-menu-menu-feedback" class="block block-menu contextual-links-region">
 
     
- <div class="panel-title-div"><h2>Feedback</h2></div>
+<h2>Feedback</h2>
 
   
-  <div class="content">
+  
     <ul class="menu"><li class="first leaf"><a href="mailto:richard.vonrauchhaupt@stonybrook.edu" title="">E-mail</a></li>
 
 </ul>  </div>
-</div>
-		</div>
 		
 		
 		<div id="social-footer" class="social-footer">
-			    <div id="block-block-30" class="block block-block contextual-links-region">
-  <div class="content">
-    <div class="social-icon odd"><a href="https://www.facebook.com/sbudoit"><img src="/stylesheets/images/social/facebook.png" /></a></div>
+			    <div class="social-icon odd"><a href="https://www.facebook.com/sbudoit"><img src="/stylesheets/images/social/facebook.png" /></a></div>
 <div class="social-icon even"><a href="https://twitter.com/sbudoit"><img src="/stylesheets/images/social/twitter.png" /></a></div>
 <div class="social-icon odd"><a href="http://www.youtube.com/channel/UCbOzIblxmg-Ing9NSEms8Tw"><img src="/stylesheets/images/social/youtube.png" /></a></div>
 <div class="social-icon even"><a href="https://plus.google.com/u/0/102779299295288343882/posts"><img src="/stylesheets/images/social/google-plus.png" /></a></div>
@@ -60,32 +47,20 @@
 <div class="social-icon odd"><a href="http://instagram.com/sbudoit"><img src="/stylesheets/images/social/instagram.png" /></a></div>
 <div class="social-icon even"><a href="https://www.yammer.com/stonybrook.edu/"><img src="/stylesheets/images/social/yammer.png" /></a></div>
   </div>
-</div>
-		</div>
 	
 		
 		<div id="bottom-footer">
-			    <div id="block-block-3" class="block block-block contextual-links-region">
+ 
 
-   
-  <div class="content">
     <p style="text-align: center;">Copyright &copy; 2008-2016 Stony Brook University Division of Information Technology - All Rights Reserved</p><p style="text-align: center;">Phone: (631) 632-9800 | Email: <a href="mailto:supportteam@stonybrook.edu">supportteam@stonybrook.edu</a></p>  </div>
+
+	</div>
+
 </div>
-<div id="block-menu-block-12" class="block block-menu-block contextual-links-region">
+  
 
-  <div class="content">
-    <div class="menu-block-wrapper menu-block-12 menu-name-menu-footer-menu parent-mlid-0 menu-level-1">
 
-</ul></div>
-  </div>
-</div>
-		</div>		 
-	</div>		
-	 </div>
-
-</div> 
-
-</div> 
+</div> <!--closes page -->
 
  <script src="/javascripts/querystring-helper.js"></script> 
 
