c9afe534df78e073a264ffa0aeba70070c9f4122
Richard von Rauchhaupt <Richard.vonRauchhaupt@stonybrook.edu>
2015-05-28 17:01:41 -0400

    removed redundant accordion files, updated styleguide menu, removed accordion-hover, not sure where that came from?

--- js/footer/custom/ready.js
+++ js/footer/custom/ready.js
@@ -7,3 +7,37 @@ $(document).ready(function() {
 
 })(jQuery);
 
+
+/* Scalable use of accordion-style slideUp/slideDown with hide-accessible */
+
+   
+/* drop-accordion */
+
+ (function( $ ) {
+
+var accordionContent;
+var openTab;
+$('.drop-accordion .tab').on('click',function() {
+  accordionContent = $(this).closest('.item').find('.accordion-content');
+  openTab = $(this).closest('.drop-accordion').find('.open').closest('.item').find('.tab');
+
+  if(accordionContent.hasClass('hide-accessible')) {
+    $(this).addClass('active');
+    accordionContent.slideUp(0,function(){
+      accordionContent.removeClass('hide-accessible').addClass('open')
+        .slideDown('fast');
+      openTab.click();
+    });
+
+  } else {
+    $(this).removeClass('active');
+    accordionContent.slideUp('fast',function(){
+      accordionContent.addClass('hide-accessible').removeClass('open')
+        .slideDown(0);
+    });
+
+  }
+
+});
+
+})(jQuery);
