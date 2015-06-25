f315b42556ca26b1052f09e9f798f861941c46f8
Richard von Rauchhaupt <Richard.vonRauchhaupt@stonybrook.edu>
2015-06-24 22:47:28 -0400

    sections, themes

--- gulpfile.js
+++ gulpfile.js
@@ -140,14 +140,14 @@ gulp.task('styles', function() {
             sourcemap: false
         })).on('error', gutil.log)
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(gulp.dest('.dist/css'));
+        .pipe(gulp.dest('.dist/css'))
         //.pipe(gulp.dest('app/assets/temp'))
         
-        notify({
+        .pipe(notify({
             message: 'Styles task complete'
-        });
+        }));
 
-        
+
 });
 
 gulp.task('browser-sync', function() {
