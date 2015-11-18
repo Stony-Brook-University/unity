2309ff0b692097f3dc6696624115008cd823d815
Richard von Rauchhaupt @ DOIT-COMM-01 <Richard.VonRauchhaupt@stonybrook.edu>
2015-11-18 14:34:49 -0500

    hero button active/visited fix

--- js/footer/custom/ready.js
+++ js/footer/custom/ready.js
@@ -21,26 +21,6 @@
 
 })(jQuery);
 
-/* jshint ignore:start */
-/* Crossfade Images for Homepage */
-
-function cycleImages(){
-      var $active = $('#cycler .active');
-      var $next = ($active.next().length > 0) ? $active.next() : $('#cycler img:first');
-      $next.css('z-index',2);//move the next image up the pile
-      $active.fadeOut(1500,function(){//fade out the top image
-    $active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
-          $next.css('z-index',3).addClass('active');//make the next image the top one
-      });
-    }
-
-$(document).ready(function(){
-// run every 7s
-setInterval('cycleImages()', 7000);
-})
-/* jshint ignore:end */
-
-
 
 /* Scalable use of accordion-style slideUp/slideDown with hide-accessible */
   
