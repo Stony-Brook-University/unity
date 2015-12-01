b30540fdbb1bc5d1c3f2a0618ea6bf05ac589a75
Rich von Rauchhaupt <rich@Richs-MacBook-Pro.local>
2015-12-01 17:03:59 -0500

    gulpfile cleanup, moved production styles task into main styles task

--- gulpfile.js
+++ gulpfile.js
@@ -20,139 +20,80 @@ var template = require('lodash.template');
 var through = require('through2');
 var directoryMap = require("gulp-directory-map");
 
-gulp.task('cleantemp', function() {
-    return gulp.src('.temp', {
-            read: false
-        })
-        .pipe(clean({
-            force: true
-        }));
-});
 
-gulp.task('cleandist', function() {
-    return gulp.src('.dist', {
-            read: false
-        })
-        .pipe(clean({
-            force: true
-        }));
-});
-
-// Lint Task
-gulp.task('lint', function() {
-    return gulp.src('js/**/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'))
-        .pipe(jshint.reporter('gulp-jshint-html-reporter', {
-            filename: __dirname + '/styleguide/compiled-assets/jshint-output.html'
-        }));
+var config = {
         
-});
+    dest: '.dist',
+
+    sass: {
+        src: './scss/**/*.{scss,sass}',
+        dest: './.dist/css'
+    },
+
+    fonts: {
+        src: './fonts/**/*',
+        dest: './.dist/assets/fonts'
+    },
+    //css:'./css',
+    //js:'./js',
+    images: {
+        src: './images/*.{png,gif,jpeg,jpg,svg}',
+        dest: './.dist/assets/images'
+    },
+
+    jsheader: 
+    {
+        src: ['js/header/libraries/*.js', 'js/header/custom/*.js'],
+        dest: '.dist/js/header'
+    },
+
+    jsfooter:
+    {
+        src: ['js/footer/libraries/*.js', 'js/footer/custom/*.js'],
+        dest: '.dist/js/footer'
+    }
+      
+};
+
+
 
 // Concatenate & Minify JS
 gulp.task('headerjs', function() {
-    return gulp.src(['js/header/libraries/*.js', 'js/header/custom/*.js'])
+    return gulp.src(config.jsheader.src)
         .pipe(concat('header.dev.js'))
-        .pipe(gulp.dest('.dist/js/header'))
+        .pipe(gulp.dest(config.jsheader.dest))
         .pipe(rename('header.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('.dist/js/header'));
+        .pipe(gulp.dest(config.jsheader.dest));
 });
 
 // Concatenate & Minify JS
 gulp.task('footerjs', function() {
-    return gulp.src(['js/footer/libraries/*.js', 'js/footer/custom/*.js'])
+    return gulp.src(config.jsfooter.src)
         .pipe(concat('footer.dev.js'))
-        .pipe(gulp.dest('.dist/js/footer'))
+        .pipe(gulp.dest(config.jsfooter.dest))
         .pipe(rename('footer.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('.dist/js/footer'));
+        .pipe(gulp.dest(config.jsfooter.dest));
 });
 
 gulp.task('fonts', function() {
-    return gulp.src('fonts/**/*')
-        .pipe(gulp.dest('.dist/assets/fonts'));
+    return gulp.src(config.fonts.src)
+        .pipe(gulp.dest(config.fonts.dest));
 });
 
 gulp.task('images', function() {
-    return gulp.src('images/**/*')
-        .pipe(gulp.dest('.dist/assets/images'));
-});
-
-gulp.task('textures-json', function() {
-
-    gulp.src(['images/backgrounds/textures/*.jpg', 'images/backgrounds/textures/*.png'])
-        .pipe(directoryMap({
-        filename: 'textures.json'
-    }))
-    .pipe(gulp.dest('styleguide/compiled-assets'));
-
-});
-
-gulp.task('patterns-json', function() {
-
-    gulp.src(['images/backgrounds/patterns/**/*.jpg', 'images/backgrounds/patterns/**/*.png'])
-        .pipe(directoryMap({
-        filename: 'patterns.json'
-    }))
-    .pipe(gulp.dest('styleguide/compiled-assets'));
-
+    return gulp.src(config.images.src)
+        .pipe(gulp.dest(config.images.dest));
 });
 
 
-// output once in markdown and then output a json file as well 
-gulp.task('todo-scss', function() {
-    gulp.src('scss/**/*.scss')
-        .pipe(todo({fileName: 'todo-scss.md' }))
-        .pipe(gulp.dest('./styleguide/compiled-assets/')) //output todo.md as markdown 
-});
 
-gulp.task('todo-js', function() {
-    gulp.src('js/**/*.js')
-        .pipe(todo({fileName: 'todo-js.md' }))
-        .pipe(gulp.dest('./styleguide/compiled-assets/')) //output todo.md as markdown 
-});
-
-gulp.task('todo-html', function() {
-    gulp.src('styleguide/**/*.html')
-        .pipe(todo({fileName: 'todo-html.md' }))
-        .pipe(gulp.dest('./styleguide/compiled-assets/')) //output todo.md as markdown 
-});
-
-gulp.task('styles-prod', ['styles'], function() {
-    gulp.src('./.dist/css/style.css')
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(minifycss())
-        .pipe(gulp.dest('.dist/css'));
-        
-    
-
-
-    gulp.src('./.dist/css/style.min.css')
-        .pipe(rename({
-            suffix: '.blessed.ie89'
-        }))
-        .pipe(bless())
-        .pipe(gulp.dest('.dist/css'));
-        
-    
-
-    gulp.src('./.dist/css/style.min.css')
-        .pipe(mqRemove({ width: "1280px" }))
-        .pipe(rename({
-            suffix: '.blessed.ie7'
-        }))
-        .pipe(bless())
-        .pipe(gulp.dest('.dist/css'));
-        
-});
 
 gulp.task('styles', function() {
-    gulp.src('./scss/*.scss')
+    gulp.src(config.sass.src)
         .pipe(sass({
-            css: '.dist/css',
+            css: config.sass.dest,
             sass: 'scss',
             fonts: 'fonts',
             debug: true,
@@ -162,12 +103,29 @@ gulp.task('styles', function() {
             sourcemap: false
         })).on('error', gutil.log)
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(gulp.dest('.dist/css'))
+        .pipe(gulp.dest(config.sass.dest))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(minifycss())
+        .pipe(gulp.dest(config.sass.dest))
+        .pipe(rename({
+            suffix: '.blessed.ie89'
+        }))
+        .pipe(bless())
+        .pipe(gulp.dest(config.sass.dest))
+        .pipe(mqRemove({ width: "1280px" }))
+        .pipe(rename({
+            suffix: '.blessed.ie7'
+        }))
+        .pipe(bless())
+        .pipe(gulp.dest(config.sass.dest));
+
         //.pipe(gulp.dest('app/assets/temp'))
         
-        .pipe(notify({
-            message: 'The Baron says: I can not handle all this Sass.'
-        }));
+        //.pipe(notify({
+        //    message: 'The Baron says: I can not handle all this Sass.'
+        //}));
 
 
 });
@@ -182,26 +140,18 @@ gulp.task('browser-sync', function() {
 
 
 gulp.task('watch', function() {
-    gulp.watch('js/header**/*.js', ['lint', 'headerjs']);
-    gulp.watch('js/footer/**/*.js', ['lint', 'footerjs']);
+ //   gulp.watch('js/header**/*.js', ['lint', 'headerjs']);
+ //   gulp.watch('js/footer/**/*.js', ['lint', 'footerjs']);
     gulp.watch('fonts/**/*', ['fonts']);
     gulp.watch('images/**/*', ['images']);
     gulp.watch('scss/**/*.scss', ['styles']);
 });
 
 
-gulp.task('watch-simple', function() {
-    gulp.watch('js/header**/*.js', ['headerjs']);
-    gulp.watch('js/footer/**/*.js', ['footerjs']);
-    gulp.watch('scss/**/*.scss', ['styles']);
-});
-
-gulp.task('default', ['lint', 'fonts', 'images', 'styles', 'todo-scss', 'todo-js', 'headerjs', 'footerjs', 'watch']);
-
-gulp.task('simple', ['styles', 'headerjs', 'footerjs', 'watch-simple']);
 
 
-gulp.task('release', ['fonts', 'images', 'styles', 'styles-prod', 'headerjs', 'footerjs']);
+gulp.task('default', ['fonts', 'images', 'styles', 'headerjs', 'footerjs', 'watch']);
+//gulp.task('default', ['lint', 'fonts', 'images', 'styles', 'todo-scss', 'todo-js', 'headerjs', 'footerjs', 'watch']);
 
 gulp.task('browsersync', ['browser-sync'], function() {
 
